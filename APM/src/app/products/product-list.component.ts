b33ddcc9618912aafb27c3d4033e674ff3a92b25
Milos Stojanovic <milos-stojanovic@hotmail.com>
2021-03-01 20:04:39 +0100

    add images

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -6,7 +6,9 @@ import {Component} from '@angular/core'
 })
 
 export class ProductListComponent {
-    pageTitle: string = 'Product List';
+    pageTitle = 'Product List';
+    imageWidth = 50;
+    imageMargin = 2;
     products: any[] = [
         {
             "productId": 2,
